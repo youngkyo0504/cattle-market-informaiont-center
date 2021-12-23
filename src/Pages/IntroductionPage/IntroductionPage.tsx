58bbd405cb6d35074cf774152b0da1b12a03cfd9
kyo-young <keumky2@gmail.com>
2021-12-23 11:41:18 +0900

    [FEATURE] scroll event

--- src/Pages/IntroductionPage/IntroductionPage.tsx
+++ src/Pages/IntroductionPage/IntroductionPage.tsx
@@ -1,6 +1,40 @@
 import React from "react";
+import ScrollAnimation from "react-animate-on-scroll";
+import styled from "styled-components/macro";
+
+const Dd = styled.div`
+  height: 100vh;
+`;
+
+const Reveal: React.SFC = ({ children }) => (
+  <ScrollAnimation
+    animateIn={"fadeIn"}
+    animateOut={"fadeOut"}
+    duration={2}
+    // offset={400}
+  >
+    {children}
+  </ScrollAnimation>
+);
+
 const IntroductionPage = () => {
-  return <div>회사소개 페이지</div>;
+  return (
+    <>
+      <div>회사 소개 페이지</div>
+      <Dd>안녕</Dd>
+      <Reveal>Hi</Reveal>
+      <ScrollAnimation
+        animateIn="fadeIn"
+        animateOut="fadeOut"
+        duration={0.5}
+        delay={0}
+        // initiallyVisible={true}
+      >
+        <h1>React Animate On Scroll</h1>
+        <h2>Using:</h2>
+      </ScrollAnimation>
+    </>
+  );
 };
 
 export default IntroductionPage;
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+// import "animate.css/animate.css";
 import GlobalStyle from "./assets/styles/global-styles";
 ReactDOM.render(
   <React.StrictMode>
